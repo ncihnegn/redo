9ecf6025d524f5576a2a1c11c134d72b29c95b46
ncihnegn <ncihnegn@users.noreply.github.com>
2019-09-29 01:18:59 -0700

    Get around stack ghc empty output by switching to gcc

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,9 @@
 module Main where
 
-import System.Directory (renameFile)
+import System.Directory (removeFile, renameFile)
 import System.Environment (getArgs)
+import System.Exit (ExitCode(..))
+import System.IO (hPutStrLn, stderr)
 import System.Process (createProcess, shell, waitForProcess)
 
 main :: IO()
@@ -13,6 +15,9 @@ redo :: String-> IO ()
 redo target = do
   let tmp = target ++ "---redoing"
   (_, _, _, ph) <- createProcess $ shell $ "sh " ++ target ++ ".do - - " ++ tmp ++ " > " ++ tmp
-  _ <- waitForProcess ph
-  renameFile tmp target
-  return()
+  exit <- waitForProcess ph
+  case exit of
+    ExitSuccess -> renameFile tmp target
+    ExitFailure code -> do
+      hPutStrLn stderr $ "Redo script exited with non-zero exit code " ++ show code
+      removeFile tmp
--- redo.do
+++
@@ -1,1 +0,0 @@
-stack ghc -- app/Main.hs -v0 -o $3
