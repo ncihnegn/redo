487b01970ee951329c74daa9675ebac8353d5f02
ncihnegn <ncihnegn@users.noreply.github.com>
2019-09-29 13:36:50 -0700

    Rewrite main

--- app/Main.hs
+++ app/Main.hs
@@ -10,9 +10,7 @@ import           System.IO          (hPutStrLn, stderr)
 import           System.Process     (createProcess, shell, waitForProcess)
 
 main :: IO ()
-main = do
-  args <- getArgs
-  mapM_ redo args
+main = getArgs >>= \ args -> mapM_ redo args
 
 redo :: String -> IO ()
 redo target = do
