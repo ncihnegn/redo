2940d48ee39a243a0843f85f305ada8950bc3295
ncihnegn <ncihnegn@users.noreply.github.com>
2019-10-04 01:26:51 -0700

    Check exception

--- app/Main.hs
+++ app/Main.hs
@@ -1,24 +1,29 @@
 module Main where
 
-import           Control.Monad      (filterM, liftM)
+import           Control.Exception  (SomeException (..), catch)
+import           Control.Monad      (filterM, liftM, unless)
 import           Data.Map.Lazy      (adjust, fromList, insert, toList)
 import           Data.Maybe         (listToMaybe)
---import           Debug.Trace        (traceShow)
-import           System.Directory   (doesFileExist, removeFile, renameFile)
+import           Data.Typeable      (typeOf)
+import           Debug.Trace        (traceShow)
+import           System.Directory   (doesFileExist, getDirectoryContents,
+                                     removeFile, renameFile)
 import           System.Environment (getArgs, getEnvironment)
 import           System.Exit        (ExitCode (..))
 import           System.FilePath    (hasExtension, replaceBaseName,
-                                     takeBaseName)
+                                     takeBaseName, (</>))
 import           System.IO          (hPutStrLn, stderr)
 import           System.Process     (createProcess, env, shell, waitForProcess)
 
---traceShow' arg = traceShow arg arg
+traceShow' arg = traceShow arg arg
 
 main :: IO ()
 main = mapM_ redo =<< getArgs
 
 redo :: String -> IO ()
-redo target = maybe printMissing redo' =<< redoPath target
+redo target = do
+  upToDate' <- upToDate target
+  unless upToDate' $ maybe printMissing redo' =<< redoPath target
   where
     printMissing = error $ "No .do file found for target " ++ target
     redo' path = do
@@ -48,3 +53,17 @@ redoPath target = listToMaybe `liftM` filterM doesFileExist candidates
     candidates =
       (target ++ ".do") :
         [replaceBaseName target "default" ++ ".do" | hasExtension target]
+
+upToDate :: String -> IO Bool
+upToDate target = do
+  deps <- getDirectoryContents depDir
+  (traceShow' . all id) `liftM` mapM depUpToDate deps
+  where
+    depDir = ".redo" </> target
+    depUpToDate :: FilePath -> IO Bool
+    depUpToDate dep =
+      catch
+        (do oldMD5 <- traceShow' `liftM` readFile (depDir </> dep)
+            return False)
+        (\(SomeException e) -> do hPutStrLn stderr $ show $ typeOf e
+                                  return False)
